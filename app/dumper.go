69ae22ce2e5a108d9f95699317f7f1a71454e46b
Paolo Galeone <nessuno@nerdz.eu>
2023-04-13 20:14:15 +0200

    Dumper.userActivityLogList: ok

--- app/dumper.go
+++ app/dumper.go
@@ -4,6 +4,9 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
+	"net/url"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/galeone/fitbit"
@@ -103,6 +106,9 @@ func (d *dumper) userActivityLogList(after *time.Time) (err error) {
 	var sort string
 	var AfterDate fitbit_types.FitbitDateTime
 	var BeforeDate fitbit_types.FitbitDateTime
+	var limit int64 = 100
+	var offset int64 = 0
+
 	if after != nil {
 		sort = "asc"
 		AfterDate.Time = *after
@@ -114,129 +120,161 @@ func (d *dumper) userActivityLogList(after *time.Time) (err error) {
 	pagination := fitbit_types.Pagination{
 		AfterDate:  AfterDate,
 		BeforeDate: BeforeDate,
-		Limit:      100,
+		Limit:      limit,
 		Next:       "",
-		Offset:     0,
+		Offset:     offset,
 		Previous:   "",
 		Sort:       sort,
 	}
 
-	// TODO: ripeti richiesta in loop finché la pagination ritornata ha senso
-	// prendi tutti i prima e tutti i dopo (autoconfigurato secondo)
-	// la prima richiesta fatta
-
-	var value *fitbit_types.ActivityLogList
-	if value, err = d.fb.UserActivityLogList(&pagination); err != nil {
-		return
-	}
-
-	for _, activity := range value.Activities {
-		activityRow := types.ActivityLog{}
-		if err = _db.First(&activityRow, activity.LogID); err == nil {
-			fmt.Println("skipping activity ", activity.LogID, ": already present")
-			continue
-		}
-
-		tx := _db.Begin()
-		// There are activities without active zone minutes
-		if activity.ActiveZoneMinutes.TotalMinutes > 0 {
-			activeZoneMinutes := types.ActiveZoneMinutes{
-				ActiveZoneMinutes: activity.ActiveZoneMinutes,
-			}
-
-			if err = tx.Create(&activeZoneMinutes); err != nil {
-				fmt.Println(err)
+	for {
+		var value *fitbit_types.ActivityLogList
+		if value, err = d.fb.UserActivityLogList(&pagination); err != nil {
+			if strings.Contains(err.Error(), "504") {
+				// NOTE: Fitbit API is a shit.
+				// It only allows us to request the latest 100 activities.
+				// Thus, if we are asking for other activities it doesn't return a meaningful error
+				// But it returns a Gateway Timeout (WTF).
 				break
 			}
-			for _, minInHRZone := range activity.ActiveZoneMinutes.MinutesInHeartRateZones {
-				minInHRZoneRow := types.MinutesInHeartRateZone{
-					MinutesInHeartRateZone: minInHRZone,
-					ActiveZoneMinutesID:    activeZoneMinutes.ID,
-				}
-				if err = tx.Create(&minInHRZoneRow); err != nil {
-					fmt.Println(err)
-					break
-				}
-			}
-			activityRow.ActiveZoneMinutesID = sql.NullInt64{
-				Int64: activeZoneMinutes.ID,
-				Valid: true,
-			}
+			return
 		}
-
-		// There are activities without source
-		if activity.Source != nil {
-			var source types.LogSource // NOTE: First requires the dest field to be zero to work correctly
-			// If not present, then create
-			if err = tx.First(&source, activity.Source.ID); err != nil {
-				source = types.LogSource{
-					LogSource: *activity.Source,
-					ID:        activity.Source.ID,
-				}
-				if err = tx.Create(&source); err != nil {
-					fmt.Println(err)
-					break
-				}
-			}
-
-			// Handle optional FK
-			activityRow.SourceID = sql.NullString{
-				String: source.ID,
-				Valid:  true,
-			}
-
-		}
-
-		// Primary Key (not serial)
-		activityRow.LogID = activity.LogID
-		// All the retrieved fields
-		activityRow.ActivityLog = activity
-		// Non optional FKs: child already created
-		activityRow.UserID = d.User.ID
-		// Overwritten time fields
-		activityRow.OriginalStartTime = activity.OriginalStartTime.Time
-		activityRow.StartTime = activity.StartTime.Time
-		// Fields that the API for some reason puts on a different type, but have a 1:1 relationship
-		// with the activity, and so they have been merged
-		activityRow.ManualInsertedCalories = activity.ManualValuesSpecified.Calories
-		activityRow.ManualInsertedSteps = activity.ManualValuesSpecified.Steps
-		activityRow.ManualInsertedDistance = activity.ManualValuesSpecified.Distance
-
-		if err = tx.Create(&activityRow); err != nil {
-			fmt.Println(err)
+		if len(value.Activities) == 0 {
 			break
 		}
 
-		// Once we have the activity stored, we can save the array types returned by the API
-		for _, activityLevel := range activity.ActivityLevel {
-			activityLevelRow := types.LoggedActivityLevel{
-				LoggedActivityLevel: activityLevel,
-				ActivityLogID:       activityRow.LogID,
+		for _, activity := range value.Activities {
+			activityRow := types.ActivityLog{}
+			if err = _db.First(&activityRow, activity.LogID); err == nil {
+				// fmt.Println("skipping activity ", activity.LogID, ": already present")
+				continue
 			}
-			if err = tx.Create(&activityLevelRow); err != nil {
-				fmt.Println(err)
-				break
-			}
-		}
 
-		for _, hrZone := range activity.HeartRateZones {
-			hrZoneRow := types.HeartRateZone{
-				HeartRateZone: hrZone,
-				ActivityLogID: sql.NullInt64{
-					Int64: activityRow.LogID,
+			tx := _db.Begin()
+			// There are activities without active zone minutes
+			if activity.ActiveZoneMinutes.TotalMinutes > 0 {
+				activeZoneMinutes := types.ActiveZoneMinutes{
+					ActiveZoneMinutes: activity.ActiveZoneMinutes,
+				}
+
+				if err = tx.Create(&activeZoneMinutes); err != nil {
+					fmt.Println(err)
+					break
+				}
+				for _, minInHRZone := range activity.ActiveZoneMinutes.MinutesInHeartRateZones {
+					minInHRZoneRow := types.MinutesInHeartRateZone{
+						MinutesInHeartRateZone: minInHRZone,
+						ActiveZoneMinutesID:    activeZoneMinutes.ID,
+					}
+					if err = tx.Create(&minInHRZoneRow); err != nil {
+						fmt.Println(err)
+						break
+					}
+				}
+				activityRow.ActiveZoneMinutesID = sql.NullInt64{
+					Int64: activeZoneMinutes.ID,
 					Valid: true,
-				},
+				}
 			}
-			if err = tx.Create(&hrZoneRow); err != nil {
+
+			// There are activities without source
+			if activity.Source != nil {
+				var source types.LogSource // NOTE: First requires the dest field to be zero to work correctly
+				// If not present, then create
+				if err = tx.First(&source, activity.Source.ID); err != nil {
+					source = types.LogSource{
+						LogSource: *activity.Source,
+						ID:        activity.Source.ID,
+					}
+					if err = tx.Create(&source); err != nil {
+						fmt.Println(err)
+						break
+					}
+				}
+
+				// Handle optional FK
+				activityRow.SourceID = sql.NullString{
+					String: source.ID,
+					Valid:  true,
+				}
+
+			}
+
+			// Primary Key (not serial)
+			activityRow.LogID = activity.LogID
+			// All the retrieved fields
+			activityRow.ActivityLog = activity
+			// Non optional FKs: child already created
+			activityRow.UserID = d.User.ID
+			// Overwritten time fields
+			activityRow.OriginalStartTime = activity.OriginalStartTime.Time
+			activityRow.StartTime = activity.StartTime.Time
+			// Fields that the API for some reason puts on a different type, but have a 1:1 relationship
+			// with the activity, and so they have been merged
+			activityRow.ManualInsertedCalories = activity.ManualValuesSpecified.Calories
+			activityRow.ManualInsertedSteps = activity.ManualValuesSpecified.Steps
+			activityRow.ManualInsertedDistance = activity.ManualValuesSpecified.Distance
+
+			if err = tx.Create(&activityRow); err != nil {
+				fmt.Println(err)
+				break
+			}
+
+			// Once we have the activity stored, we can save the array types returned by the API
+			for _, activityLevel := range activity.ActivityLevel {
+				activityLevelRow := types.LoggedActivityLevel{
+					LoggedActivityLevel: activityLevel,
+					ActivityLogID:       activityRow.LogID,
+				}
+				if err = tx.Create(&activityLevelRow); err != nil {
+					fmt.Println(err)
+					break
+				}
+			}
+
+			for _, hrZone := range activity.HeartRateZones {
+				hrZoneRow := types.HeartRateZone{
+					HeartRateZone: hrZone,
+					ActivityLogID: sql.NullInt64{
+						Int64: activityRow.LogID,
+						Valid: true,
+					},
+				}
+				if err = tx.Create(&hrZoneRow); err != nil {
+					fmt.Println(err)
+					break
+				}
+			}
+
+			if err = tx.Commit(); err != nil {
 				fmt.Println(err)
 				break
 			}
 		}
 
-		if err = tx.Commit(); err != nil {
+		if nextURL, err := url.Parse(value.Pagination.Next); err != nil {
 			fmt.Println(err)
 			break
+		} else {
+			next := nextURL.Query()
+			offset, _ = strconv.ParseInt(next.Get("offset"), 10, 64)
+			if afterDate := next.Get("afterDate"); afterDate != "" {
+				AfterDate.Time, _ = time.Parse(fitbit_types.DateTimeLayout, afterDate)
+			}
+			if beforeDate := next.Get("beforeDate"); beforeDate != "" {
+				BeforeDate.Time, _ = time.Parse(fitbit_types.DateTimeLayout, beforeDate)
+			}
+			pagination = fitbit_types.Pagination{
+				AfterDate:  AfterDate,
+				BeforeDate: BeforeDate,
+				Limit:      limit,
+				Next:       "",
+				Offset:     offset,
+				Previous:   "",
+				Sort:       sort,
+			}
 		}
+
 	}
 	return
 }
@@ -251,6 +289,9 @@ func (d *dumper) Dump(after *time.Time) error {
 	// because Fitbit allows to get only the daily data.
 
 	// fmt.Println(d.userActivityDailyGoal())
+
+	// NOTE: this is not a dump ALL activities. But only the latest 100 activities
+	// because hte Fitbit API limit (for no reason) this endpoint data.
 	fmt.Println(d.userActivityLogList(nil))
 	return nil
 }
