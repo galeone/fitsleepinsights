aaafffdf35d2837ecd71e65ab4d76900b7a73afe
Paolo Galeone <nessuno@nerdz.eu>
2023-05-26 21:31:07 +0200

    dataset: create csv with vertex.ai requried columns

--- app/dataset.go
+++ app/dataset.go
@@ -4,22 +4,28 @@ import (
 	"bytes"
 	"encoding/csv"
 	"errors"
+	"strconv"
 )
 
 // userDataToCSV converts the slice of UserData to a CSV string
+// Add a column ID, because vertex.ai requires it. It's not used for training
+// The label column will be decided by the user ideally. Right now we use the sleep efficiency
+// The date is already in the csv in the vertex.ai expected format, so we don't need to add it.
+// ref: https://cloud.google.com/vertex-ai/docs/tabular-data/bp-tabular
 func userDataToCSV(userData []*UserData) (ret string, err error) {
 	if len(userData) == 0 {
 		return ret, errors.New("empty userData slice")
 	}
 
-	headers := userData[0].Headers()
+	headers := []string{"ID"}
+	headers = append(headers, userData[0].Headers()...)
 
 	buffer := bytes.NewBufferString("")
 	w := csv.NewWriter(buffer)
 	w.Write(headers)
 
-	for _, u := range userData {
-		if err := w.Write(u.Values()); err != nil {
+	for id, u := range userData {
+		if err := w.Write(append([]string{strconv.FormatInt(int64(id), 10)}, u.Values()...)); err != nil {
 			return ret, err
 		}
 	}
