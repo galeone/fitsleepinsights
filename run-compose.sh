938b20e4da0b9fa20453d594740cc7c7d146fb7d
Harsh Manvar <harsh.manvar111@gmail.com>
2024-04-11 15:22:11 +0530

    adding the docker-compose for local test

---
+++ run-compose.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+docker pull pgvector/pgvector:pg16
+
+mkdir -p volume
+
+if [ ! -d "$PWD/volume/postgresql" ]; then
+   mkdir -p volume/postgresql/data
+   chmod 755 -R $PWD/volume/postgresql/
+fi
+
+if [ -z $1 ]; then
+  chmod 755 -R $PWD/volume/
+  docker-compose -f docker-compose.yaml up -d
+elif [[ $1 = "up" ]]; then
+  chmod 755 -R $PWD/volume/
+elif [[ $1 = "down" ]]; then
+  docker-compose -f docker-compose.yaml down
+fi
